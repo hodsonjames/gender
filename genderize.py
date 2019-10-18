6760aafd7376f721123d20c0c3c0f493a3444ef6
PLivdan <51764289+PLivdan@users.noreply.github.com>
2019-10-18 15:10:07 -0700

    Updated Code

--- genderize.py
+++ genderize.py
@@ -23,6 +23,7 @@ get_data("filename")
 def split(values): 
     return [char for char in values] 
 
+#removes vowels from features
 def remove_vowels(s):
     vowels = "aeiouAEIOU"
     s_without_vowels = ""
@@ -31,6 +32,7 @@ def remove_vowels(s):
             s_without_vowels += letter
     return s_without_vowels
 
+#removes consonants from features
 def only_vowels(s):
     vowels = "aeiouAEIOU"
     only_vowels = ""
@@ -39,8 +41,8 @@ def only_vowels(s):
             only_vowels += letter
     return only_vowels
 
+#extracts features from name 
 def extract_features(name):
-    
     name = name.lower()
     return {
         'last_char': name[-1],
@@ -52,18 +54,29 @@ def extract_features(name):
         'no_vowels': remove_vowels(name),
         'only_vowels': only_vowels(name),
     }
-
-
-    
-class genderizer:
+            
+class Genderizer:
 
     def __init__(self):
-        with open(get_data("country_pop.csv")) as g:
-            self.pop = dict(filter(None, csv.reader(g)))
+        self.genderDict = {}
+        self.listmale = []
+        self.listfem = []
         
-        self.f = open(get_data("nam_dict.txt"))
+        self.buildDict()
+        self.trainClassifier()
         
-        self.countries = u"""great_britain ireland usa italy malta portugal spain france
+#Initializes dictionary for names                
+    def buildDict(self):
+        genderDict = {}
+        shortNames = []
+        listmale = []
+        listfem =[]  
+        Count = 0          
+        #Acceses country_pop to get current population estimates for the population of each country
+        pop = dict(filter(None, csv.reader(open(get_data("country_pop.csv")))))
+        f = open(get_data("nam_dict.txt"))        
+        #List of countries from 
+        countries = u"""great_britain ireland usa italy malta portugal spain france
                            belgium luxembourg the_netherlands east_frisia germany austria
                            swiss iceland denmark norway sweden finland estonia latvia
                            lithuania poland czech_republic slovakia hungary romania
@@ -72,18 +85,10 @@ class genderizer:
                            azerbaijan georgia the_stans turkey arabia israel china india
                            japan korea vietnam other_countries
                          """.split()
-        
-        
-        self.genderDict = {}
-        self.shortNames = []
-        
-        self.listmale = []
-        self.listfem =[]
-        
-        self.count = 0
-        
-        for line in self.f:
-            if self.count >= 362:
+               
+        for line in f:
+            if Count >= 362:
+                #DOCUMENTATION_LENGTH = 362
                 text = line[0:86]
                 mf = text[:2].strip() # M,1M,?M, F,1F,?F, ?, =
                 #  =  <short_name> <long_name> 
@@ -101,84 +106,98 @@ class genderizer:
                                 else:
                                     names = [name]
                                 for name in names:
-                                    if name not in self.genderDict:
-                                        self.genderDict[name] = {}
-                                        self.genderDict[name][mf] = {}                                   
+                                    if name not in genderDict:
+                                        genderDict[name] = {}
+                                        genderDict[name][mf] = {}                                   
                                     else:  
-                                        if mf not in self.genderDict[name]:
-                                            self.genderDict[name][mf] = {} 
+                                        if mf not in genderDict[name]:
+                                            genderDict[name][mf] = {} 
                                             
-                                    for country,freq in zip(self.countries,frequencies):
+                                    for country,freq in zip(countries,frequencies):
                                         if freq != ' ':
-                                            norm_freq = int((float(self.pop[country]) * 1000) * 0.02 * (2 ** (int(freq,16) - 10)))
+                                            norm_freq = int((float(pop[country]) * 1000) * 0.02 * (2 ** (int(freq,16) - 10)))
                                             #norm_freq = int(((float(pop[country]))*1000) * (math.log(int(freq,16))))
                                             # print(int(float(pop[country]) * 1000)) 
                                             # print((math.log(int(freq,16),2)))
                                             
-                                            self.genderDict[name][mf][country] = norm_freq
+                                            genderDict[name][mf][country] = norm_freq
                
-            self.count += 1
+            Count += 1
+
         
-        for [name, frequencies] in self.shortNames:
-            shortName, longName = name.split()
-            if shortName in self.genderDict and not longName in self.genderDict:
-                for nameList in self.genderDict[shortName]:
-                    if longName in self.genderDict:
-                        self.genderDict[longName].append(nameList)
+        for [name, frequencies] in shortNames:
+            shortName, longName = name.lower().split()
+            if shortName in genderDict and not longName in genderDict:
+                for nameList in genderDict[shortName]:
+                    if longName in genderDict:
+                        genderDict[longName].append(nameList)
                     else:
-                        self.genderDict[longName] = [nameList]
+                        genderDict[longName] = [nameList]
                     
-            elif longName in self.genderDict and not shortName in self.genderDict:
-                for nameList in self.genderDict[longName]:
-                    if shortName in self.genderDict:
-                        self.genderDict[shortName].append(nameList)
+            elif longName in genderDict and not shortName in genderDict:
+                for nameList in genderDict[longName]:
+                    if shortName in genderDict:
+                        genderDict[shortName].append(nameList)
                     else:
-                        self.genderDict[shortName] = [nameList]
+                        genderDict[shortName] = [nameList]
                     
         #Sorts names for male and female with values 
                         
-        for key in self.genderDict.keys():
-                if 'M' in self.genderDict[key]:
-                    self.listmale.append(key) 
+        for key in genderDict.keys():
+                if 'M' in genderDict[key]:
+                    listmale.append(key) 
                 else:
-                    if 'F' in self.genderDict[key]:
-                        self.listfem.append(key) 
-                      
-        self.all_names = [(i, 'm') for i in self.listmale] + [(i, 'f') for i in self.listfem]
+                    if 'F' in genderDict[key]:
+                        listfem.append(key) 
+#loop to generate data for classifier   
+#while val <= len(all_names) * 0.8:
+        self.genderDict = genderDict
+        self.listmale = listmale
+        self.listfem = listfem
+
+    #Trains classifer for NLTK       
+    def trainClassifier(self):
+            all_names = [(i, 'm') for i in self.listmale] + [(i, 'f') for i in self.listfem]
+            for x in range(0, 1):
+                Val = 2000
+                #DOCUMENTATION_LENGTH = 2000
+                random.shuffle(all_names)
+                
+                #splits feature sets into training and test sets
+                test_set = all_names[Val:]
+                train_set= all_names[:Val]
+                
+                # The training set is used to train a new "naive Bayes" classifier. 
+                test_set_feat = [(extract_features(n), g) for n, g in test_set]
+                train_set_feat= [(extract_features(n), g) for n, g in train_set]
+                
+                self.classifier = nltk.NaiveBayesClassifier.train(train_set_feat)    
+                self.accuracy = nltk.classify.accuracy(self.classifier, test_set_feat)           
+
     
-    
-    #loop to generate data for classifier
-    
-    #while val <= len(all_names) * 0.8:
-    
-        for x in range(0, 1):
-            val = 2000
-            
-            random.shuffle(self.all_names)
-            
-            #splits feature sets into training and test sets
-            test_set = self.all_names[val:]
-            train_set= self.all_names[:val]
-            
-            # The training set is used to train a new "naive Bayes" classifier. 
-            test_set_feat = [(extract_features(n), g) for n, g in test_set]
-            train_set_feat= [(extract_features(n), g) for n, g in train_set]
-            
-            self.classifier = nltk.NaiveBayesClassifier.train(train_set_feat)    
-            self.accuracy = nltk.classify.accuracy(self.classifier, test_set_feat)
-        
     def genderize(self,name,location = None):
+        name = name.lower()
         for key in self.genderDict.keys():
-            if name in self.genderDict.keys() and location in self.genderDict[name]:
+#            if name in self.genderDict.keys() and location != None:
+#                if location in self.genderDict[name].keys():
+#                    if 'M' in self.genderDict[key]:        
+#                        return 'm'
+#                    else:
+#                        return 'f'
+#                    
+#                else:
+#                    print(self.genderDict[name])
+#                    return 'location not found'
+                
+            if name in self.genderDict.keys():
+                print(name)
                 if 'M' in self.genderDict[key]:
+                    print(self.genderDict[key])
                     return 'm'
                 else:
                     return 'f'
-            if name in self.genderDict.keys() and location not in self.genderDict[name]:
-                if 'M' in self.genderDict[key]:
-                    return 'm'
-                else:
-                    return 'f'
+
             if name not in self.genderDict.keys():
+                print('classified')
                 return self.classifier.classify(extract_features(name))
              
